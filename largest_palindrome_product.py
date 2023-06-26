0485711fe1b9d07df51b4a489a1cae399ca85182
manipurath-krishnapriya <118977701+manipurath-krishnapriya@users.noreply.github.com>
2023-06-26 10:16:22 +0200

    Add files via upload

---
+++ largest_palindrome_product.py
@@ -0,0 +1,24 @@
+# METHOD 1
+
+palindromes = []
+for x in range(999,100,-1):
+    for y in range(999,100,-1):
+        product = x * y
+        if (str(product) == str(product)[::-1]):
+            palindromes.append(product)  
+print("\n Method 1 \n The largest palindrome made from the product of two 3-digit numbers is " + str(max(palindromes)))
+
+#________________________________________________________________________________________________________#
+
+# METHOD 2
+
+solution = 1
+for x in range(999,100,-1):
+    for y in range(999,100,-1):
+        product = x * y
+        if (str(product) == str(product)[::-1]):
+            if (product > solution):
+                solution = product                    
+print("\n Method 2 \n The largest palindrome made from the product of two 3-digit numbers is " + str(solution))
+        
+
